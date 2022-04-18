9936f53192d9cef06f709b9dcc82f3e72fab9dce
Valentina Cupać <valentina.cupac@optivem.com>
2022-04-18 17:00:50 +0200

    #1 Setup Project - Added CI Workflow - Sample failing test

--- src/test/java/com/optivem/kata/banking/TddBankingKataApplicationTests.java
+++ src/test/java/com/optivem/kata/banking/TddBankingKataApplicationTests.java
@@ -3,11 +3,14 @@ package com.optivem.kata.banking;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class TddBankingKataApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(true).isTrue();
 	}
 
 }
