d5ece007eaf7b7d4856708271c67fad0fb998985
Valentina Cupać <valentina.cupac@optivem.com>
2022-04-18 16:53:50 +0200

    #1 Setup Project

---
+++ src/main/java/com/optivem/kata/banking/TddBankingKataApplication.java
@@ -0,0 +1,13 @@
+package com.optivem.kata.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddBankingKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddBankingKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/optivem/kata/banking/TddBankingKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optivem.kata.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddBankingKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
