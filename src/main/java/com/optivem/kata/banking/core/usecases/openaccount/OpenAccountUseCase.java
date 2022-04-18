3659a996327759b9bda5b529b7c1a32e87cb7b3a
Valentina Cupać <valentina.cupac@optivem.com>
2022-04-18 20:06:47 +0200

    #7 OpenAccountUseCase - ExceptionalFlow - Empty first name - Refactoring

--- src/main/java/com/optivem/kata/banking/core/usecases/openaccount/OpenAccountUseCase.java
+++ src/main/java/com/optivem/kata/banking/core/usecases/openaccount/OpenAccountUseCase.java
@@ -6,10 +6,14 @@ import com.optivem.kata.banking.core.domain.exceptions.ValidationMessages;
 public class OpenAccountUseCase {
 
     public OpenAccountResponse handle(OpenAccountRequest request) {
-        if(request.getFirstName() == null || request.getFirstName().trim().equals("")) {
+        if(isNullOrWhitespace(request.getFirstName())) {
             throw new ValidationException(ValidationMessages.FIRST_NAME_EMPTY);
         }
 
         return new OpenAccountResponse();
+    }
+
+    private static boolean isNullOrWhitespace(String value) {
+        return value == null || value.trim().equals("");
     }
 }
