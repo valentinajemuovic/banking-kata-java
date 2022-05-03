3df5f1f8d2fdc99da255a5a2626d8abe87423ac0
Valentina Cupać <valentina.cupac@optivem.com>
2022-05-03 15:03:40 +0200

    #22 ViewAccountUseCase - Show account score - Refactoring

--- src/main/java/com/optivem/kata/banking/core/domain/accounts/scoring/FactorAggregator.java
+++ src/main/java/com/optivem/kata/banking/core/domain/accounts/scoring/FactorAggregator.java
@@ -1,5 +1,7 @@
 package com.optivem.kata.banking.core.domain.accounts.scoring;
 
+import com.optivem.kata.banking.core.domain.accounts.BankAccount;
+
 public interface FactorAggregator {
-    int aggregate();
+    int aggregate(BankAccount bankAccount);
 }
