77adf110ef9cede55d6fd83d0c1e4d9d1147ec30
Valentina Cupać <valentina.cupac@optivem.com>
2022-06-20 11:16:43 +0200

    #32 OpenAccountUseCase - AccountId (Snowflake ID Generator)

--- src/main/java/com/optivem/kata/banking/infra/real/RandomAccountIdGenerator.java
+++ src/main/java/com/optivem/kata/banking/infra/real/RandomAccountIdGenerator.java
@@ -2,14 +2,22 @@ package com.optivem.kata.banking.infra.real;
 
 import com.optivem.kata.banking.core.domain.accounts.AccountId;
 import com.optivem.kata.banking.core.domain.accounts.AccountIdGenerator;
+import de.mkammerer.snowflakeid.SnowflakeIdGenerator;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomAccountIdGenerator implements AccountIdGenerator {
+    private static final int GENERATOR_ID = 0; // TODO: VC: Make configurable
+
+    private SnowflakeIdGenerator generator;
+
+    public RandomAccountIdGenerator() {
+        this.generator = SnowflakeIdGenerator.createDefault(GENERATOR_ID);
+    }
+
     @Override
     public AccountId next() {
-        // TODO: VC: Replace by Snowflake ID
-        var value = (long)(Math.random() * 10000000);
+        var value = generator.next();
         return AccountId.of(value);
     }
 }
