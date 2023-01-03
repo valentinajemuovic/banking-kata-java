687f65564edf0502a69cdd17ef397b9f3afde57b
Valentina Cupać <valentina.cupac@optivem.com>
2023-01-03 16:02:08 +0100

    #68 Modularization - Architecture - MongoBankAccountStorageTest inherits from BankAccountStorageTest

---
+++ adapters-persistence-mongo/src/test/java/com/optivem/kata/banking/adapters/persistence/mongo/MongoBankAccountStorageTest.java
@@ -0,0 +1,21 @@
+package com.optivem.kata.banking.adapters.persistence.mongo;
+
+import com.optivem.kata.banking.BankingApplication;
+import com.optivem.kata.banking.adapters.driven.BankAccountStorageTest;
+import com.optivem.kata.banking.core.ports.driven.AccountIdGenerator;
+import com.optivem.kata.banking.core.ports.driven.AccountNumberGenerator;
+import com.optivem.kata.banking.core.ports.driven.BankAccountStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+// TODO: VC: This should fail because Mongo not yet implemented fully
+@SpringBootTest(classes = BankingApplication.class)
+@ContextConfiguration
+public class MongoBankAccountStorageTest extends BankAccountStorageTest {
+
+    @Autowired
+    public MongoBankAccountStorageTest(BankAccountStorage storage, AccountIdGenerator accountIdGenerator, AccountNumberGenerator accountNumberGenerator) {
+        super(storage, accountIdGenerator, accountNumberGenerator);
+    }
+}
