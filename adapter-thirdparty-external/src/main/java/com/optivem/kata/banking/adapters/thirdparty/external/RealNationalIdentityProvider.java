02a2b7d67f7af7a3b421598e927dbefa3049eab7
Valentina Cupać <valentina.cupac@optivem.com>
2023-01-04 17:55:07 +0100

    #72 Third Party Integration - National Identity Provider - RealNationalIdentityProvider

--- adapter-thirdparty-external/src/main/java/com/optivem/kata/banking/adapters/thirdparty/external/RealNationalIdentityProvider.java
+++ adapter-thirdparty-external/src/main/java/com/optivem/kata/banking/adapters/thirdparty/external/RealNationalIdentityProvider.java
@@ -6,16 +6,24 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 public class RealNationalIdentityProvider implements NationalIdentityProvider {
+    private static final String URL = "https://jsonplaceholder.typicode.com"; // TODO: Move into configuration
+    private static final String PATH = "users/%s";
+
     @Override
     public boolean exists(String nationalIdentityNumber) {
-        var client = WebClient.create("https://jsonplaceholder.typicode.com");
-        var responseSpec = client.get().uri("users/ab").retrieve();
+        var client = WebClient.create(URL);
+        var path = getPath(nationalIdentityNumber);
+        var responseSpec = client.get().uri(path).retrieve();
 
         var user = responseSpec
                 .onStatus(status -> HttpStatus.NOT_FOUND.equals(status), response -> Mono.empty())
                 .bodyToMono(UserDto.class)
                 .block();
 
-        return user != null;
+        return user.getId() != null;
+    }
+
+    private String getPath(String nationalIdentityNumber) {
+        return String.format(PATH, nationalIdentityNumber);
     }
 }
