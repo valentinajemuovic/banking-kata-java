dfc2eb897871904d7243807b55a486f3e2043907
Valentina Cupać <valentina.cupac@optivem.com>
2023-01-05 11:13:10 +0100

    #74 REST API - Contract Testing - Consumer Testing

--- adapter-restapi-spring/src/test/java/com/optivem/kata/banking/adapters/restapi/spring/BankingConsumerContractTest.java
+++ adapter-restapi-spring/src/test/java/com/optivem/kata/banking/adapters/restapi/spring/BankingConsumerContractTest.java
@@ -1,11 +1,14 @@
 package com.optivem.kata.banking.adapters.restapi.spring;
 
 import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
+import com.optivem.kata.banking.core.internal.cleanarch.domain.scoring.Score;
+import com.optivem.kata.banking.core.ports.driver.accounts.viewaccount.ViewAccountResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -15,9 +18,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 @PactTestFor(providerName = "banking-provider", hostInterface = "localhost")
 public class BankingConsumerContractTest {
 
+    private static String EXISTING_ACCOUNT_NUMBER = "ABC_001";
+    private static String NON_EXISTENT_ACCOUNT_NUMBER = "999-999999-999";
+
+    private static String FULL_NAME = "John Smith";
+    private static int BALANCE = 20;
+    private static Score SCORE = Score.C;
+
     @Pact(consumer = "banking-consumer")
     public RequestResponsePact createPactForNonexistentAccountNumber(PactDslWithProvider builder) {
-        var accountNumber = "999-999999-999";
+        var accountNumber = NON_EXISTENT_ACCOUNT_NUMBER;
 
         var pathFormat = "/bank-accounts/%s";
         var path = String.format(pathFormat, accountNumber);
@@ -31,15 +41,63 @@ public class BankingConsumerContractTest {
                 .toPact();
     }
 
+    @Pact(consumer = "banking-consumer")
+    public RequestResponsePact createPactForExistingAccountNumber(PactDslWithProvider builder) {
+        var accountNumber = EXISTING_ACCOUNT_NUMBER;
+        var body = new PactDslJsonBody()
+                .stringValue("accountNumber", accountNumber)
+                .stringType("fullName", FULL_NAME)
+                .integerType("balance", BALANCE)
+                .integerType("score", SCORE.ordinal());
+
+
+        var pathFormat = "/bank-accounts/%s";
+        var path = String.format(pathFormat, accountNumber);
+
+        return builder.given("GET Bank Account: a Bank Account with the specified ID ABC_001 already exists")
+                .uponReceiving("A request for Bank Account data")
+                .path(path)
+                .method("GET")
+                .willRespondWith()
+                .status(200)
+                .body(body)
+                .toPact();
+    }
+
     @Test
     @PactTestFor(pactMethod = "createPactForNonexistentAccountNumber")
     public void should_return_none_when_bank_account_not_exists(MockServer mockServer) {
         var url = mockServer.getUrl();
         var client = new BankingClient(url);
-        var accountNumber = "999-999999-999";
+        var accountNumber = NON_EXISTENT_ACCOUNT_NUMBER;
         var response = client.viewAccount(accountNumber);
 
         assertThat(response).isEmpty();
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "createPactForExistingAccountNumber")
+    public void should_return_response_when_bank_account_exists(MockServer mockServer) {
+        var url = mockServer.getUrl();
+        var client = new BankingClient(url);
+        var accountNumber = EXISTING_ACCOUNT_NUMBER;
+        var response = client.viewAccount(accountNumber);
+
+        var expectedResponse = ViewAccountResponse.builder()
+            .accountNumber(EXISTING_ACCOUNT_NUMBER)
+            .fullName(FULL_NAME)
+            .balance(BALANCE)
+            .score(SCORE)
+            .build();
+
+        assertThat(response).isNotEmpty();
+        assertThat(response.get()).usingRecursiveComparison().isEqualTo(expectedResponse);
+
+        // assertThat(response.get().getAccountNumber()).isEqualTo(EXISTING_ACCOUNT_NUMBER);
+        // assertThat(response.get().getFullName()).isEqualTo(FULL_NAME);
+        // assertThat(response.get().getFullName()).isEqualTo(FULL_NAME);
+        // assertThat(response.get().getFullName()).isEqualTo(FULL_NAME);
+        // TODO: VC: Full comparison
     }
 }
 
@@ -50,8 +108,7 @@ public class BankingConsumerContractTest {
 
 public class RealNationalIdentityProviderConsumerContractTest {
 
-    private static int EXISTING_ID = 2;
-    private static int INVALID_ID = 99;
+
 
     @Pact(consumer = "banking-consumer")
     public RequestResponsePact createPactForExistingUserId(PactDslWithProvider builder) {
