92f87bbeb0210e747009a11a1535c7e77ae8cbce
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2023-05-25 22:02:23 -0400

    Create the adapter-persistence-jdbc

--- adapter-persistence-redis/src/main/java/com/optivem/kata/banking/adapter/driven/persistence/redis/configurations/RedisConfiguration.java
+++ adapter-persistence-redis/src/main/java/com/optivem/kata/banking/adapter/driven/persistence/redis/configurations/RedisConfiguration.java
@@ -1,13 +1,16 @@
 package com.optivem.kata.banking.adapter.driven.persistence.redis.configurations;
 
+import com.optivem.kata.banking.adapter.driven.base.ProfileNames;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
+@Profile(ProfileNames.ADAPTER_PERSISTENCE_REDIS)
 public class RedisConfiguration {
 
     @Value("${spring.redis.host}")
