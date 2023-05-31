008a346f9d000499315950756ec99e9d4954c613
Valentina Cupać <valentina.cupac@optivem.com>
2023-05-31 13:27:11 +0200

    #108 BankAccount - Event Sourcing

---
+++ core/src/main/java/com/optivem/kata/banking/core/internal/cleanarch/domain/accounts/ESBankAccount.java
@@ -0,0 +1,73 @@
+package com.optivem.kata.banking.core.internal.cleanarch.domain.accounts;
+
+import com.optivem.kata.banking.core.internal.cleanarch.domain.common.Guard;
+import com.optivem.kata.banking.core.ports.driver.exceptions.ValidationException;
+import com.optivem.kata.banking.core.ports.driver.exceptions.ValidationMessages;
+
+import java.time.LocalDate;
+
+import static com.optivem.kata.banking.core.internal.cleanarch.domain.common.Guard.guard;
+
+/***
+ * Alternative implementation of bank account - using event sourcing
+ */
+public class ESBankAccount {
+    private final AccountId accountId;
+    private final AccountNumber accountNumber;
+    private final String nationalIdentityNumber;
+    private final AccountHolderName accountHolderName;
+    private final LocalDate openingDate;
+    private Balance balance;
+
+    public ESBankAccount(AccountId accountId, AccountNumber accountNumber, String nationalIdentityNumber, AccountHolderName accountHolderName, LocalDate openingDate, Balance balance) {
+        Guard.guard(nationalIdentityNumber).againstNullOrWhitespace(ValidationMessages.NATIONAL_IDENTITY_NUMBER_EMPTY);
+        Guard.guard(accountId).againstNull(ValidationMessages.ACCOUNT_ID_EMPTY);
+        guard(accountNumber).againstNull(ValidationMessages.ACCOUNT_NUMBER_EMPTY);
+        Guard.guard(accountHolderName).againstNull(ValidationMessages.ACCOUNT_HOLDER_NAME_EMPTY);
+        Guard.guard(openingDate).againstNull(ValidationMessages.OPENING_DATE_EMPTY);
+        Guard.guard(balance).againstNull(ValidationMessages.BALANCE_EMPTY);
+
+        this.accountId = accountId;
+        this.accountNumber = accountNumber;
+        this.nationalIdentityNumber = nationalIdentityNumber;
+        this.accountHolderName = accountHolderName;
+        this.openingDate = openingDate;
+        this.balance = balance;
+    }
+
+    public ESBankAccount(BankAccount other) {
+        this(other.getAccountId(), other.getAccountNumber(), other.getNationalIdentityNumber(), other.getAccountHolderName(), other.getOpeningDate(), other.getBalance());
+    }
+
+    public AccountId getAccountId() { return accountId; }
+
+    public AccountNumber getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getNationalIdentityNumber() {
+        return nationalIdentityNumber;
+    }
+
+    public AccountHolderName getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public LocalDate getOpeningDate() { return openingDate; }
+
+    public Balance getBalance() {
+        return balance;
+    }
+
+    public void deposit(TransactionAmount amount) {
+        balance = balance.add(amount);
+    }
+
+    public void withdraw(TransactionAmount amount) {
+        if (amount.greaterThan(balance)) {
+            throw new ValidationException(ValidationMessages.INSUFFICIENT_FUNDS);
+        }
+
+        balance = balance.subtract(amount);
+    }
+}
