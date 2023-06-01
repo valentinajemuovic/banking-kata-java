bc477afb4ed6f452c226130c77cddf927e316c0f
Joao Cipriano <joaolucassalvatore@gmail.com>
2023-06-01 18:00:15 -0300

    Solve code smells in the class FakeEventBus

--- adapter-messaging-fake/src/main/java/com/optivem/kata/banking/adapter/driven/messaging/fake/FakeEventBus.java
+++ adapter-messaging-fake/src/main/java/com/optivem/kata/banking/adapter/driven/messaging/fake/FakeEventBus.java
@@ -1,7 +1,6 @@
 package com.optivem.kata.banking.adapter.driven.messaging.fake;
 
 import com.optivem.kata.banking.core.ports.driven.EventBus;
-import com.optivem.kata.banking.core.ports.driven.events.AccountOpenedDto;
 import com.optivem.kata.banking.core.ports.driven.events.EventDto;
 
 import java.util.ArrayDeque;
@@ -25,7 +24,9 @@ public class FakeEventBus implements EventBus {
     public void shouldHavePublishedExactly(EventDto expectedEvent) {
         var publishedEvents = getPublishedEvents();
         assertThat(publishedEvents.size()).isEqualTo(1);
-        assertThat(publishedEvents.stream().findFirst().get()).isEqualTo(expectedEvent);
+        assertThat(publishedEvents.stream().findFirst())
+                .isPresent()
+                .contains(expectedEvent);
     }
 
     private List<EventDto> getPublishedEvents() {
