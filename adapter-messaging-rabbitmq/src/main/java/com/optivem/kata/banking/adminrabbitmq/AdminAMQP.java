cf14477522d250e1f68037c9c3073e0296b560a4
Kalin Ivanov <kalin2578@gmail.com>
2023-10-11 15:14:18 +0300

    remove unused imports

--- adapter-messaging-rabbitmq/src/main/java/com/optivem/kata/banking/adminrabbitmq/AdminAMQP.java
+++ adapter-messaging-rabbitmq/src/main/java/com/optivem/kata/banking/adminrabbitmq/AdminAMQP.java
@@ -1,34 +1,21 @@
 package com.optivem.kata.banking.adminrabbitmq;
 
-import com.optivem.kata.banking.configuration.RabbitMQConfig;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AdminAMQP {
-
-    private RabbitAdmin rabbitAdmin;
-    private RabbitTemplate rabbitTemplate;
-
+    private final RabbitAdmin rabbitAdmin;
 
     public AdminAMQP(RabbitAdmin rabbitAdmin, RabbitTemplate rabbitTemplate){
         this.rabbitAdmin = rabbitAdmin;
-        this.rabbitTemplate = rabbitTemplate;
     }
-
 
     public RabbitAdmin getRabbitAdmin(){
         return this.rabbitAdmin;
     }
-
-    // public Exchange getExchangeByName(String exchangeName) {
-    //         rabbitAdmin.getRabbitTemplate().getEx
-    // }
-
-
 
     public void createExchange(String exchangeName){
         Exchange exchange = ExchangeBuilder.directExchange(exchangeName)
@@ -37,7 +24,6 @@ public class AdminAMQP {
 
         rabbitAdmin.declareExchange(exchange);
     }
-
 
     public void createQueueAndBind(Exchange exchange) {
 
