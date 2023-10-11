41a0a5d81209c41efc153a634cb620edd5eb31b2
Kalin Ivanov <kalin2578@gmail.com>
2023-10-11 15:08:39 +0300

    remove unused imports and change constant names

--- adapter-messaging-rabbitmq/src/main/java/com/optivem/kata/banking/RabbitMQEventBus.java
+++ adapter-messaging-rabbitmq/src/main/java/com/optivem/kata/banking/RabbitMQEventBus.java
@@ -1,12 +1,8 @@
 package com.optivem.kata.banking;
 
 import com.optivem.kata.banking.adapter.driven.base.ProfileNames;
-import com.optivem.kata.banking.adminrabbitmq.AdminAMQP;
-import com.optivem.kata.banking.configuration.RabbitMQConfig;
 import com.optivem.kata.banking.core.ports.driven.EventBus;
-import com.optivem.kata.banking.core.ports.driven.events.AccountOpenedDto;
 import com.optivem.kata.banking.core.ports.driven.events.EventDto;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -19,30 +15,24 @@ import java.time.LocalDateTime;
 @Component
 @Profile(ProfileNames.ADAPTER_MESSAGING_RABBITMQ)
 public class RabbitMQEventBus implements EventBus {
+    private final RabbitTemplate rabbitTemplate;
 
-    private RabbitTemplate rabbitTemplate;
-
-    private final String DEFAULT_EXCHANGE_NAME;
-    private final String DEFAULT_QUEUE_NAME;
-    private final String DEFAULT_ROUTINGKEY_QUEUE;
-
+    private final String defaultExchangeName;
+    private final String defaultRoutingKeyQueue;
 
     @Autowired
     public RabbitMQEventBus(@Qualifier("rabbitAMQPTemplate") RabbitTemplate rabbitTemplate,
                             @Value("${default.exchange.name}") String exchangeName,
-                            @Value("${default.queue.name}") String queueName,
                             @Value("${default.routingkey.name}") String routingKeyName){
 
         this.rabbitTemplate = rabbitTemplate;
-        this.DEFAULT_EXCHANGE_NAME = exchangeName;
-        this.DEFAULT_QUEUE_NAME = queueName;
-        this.DEFAULT_ROUTINGKEY_QUEUE = routingKeyName;
-
+        this.defaultExchangeName = exchangeName;
+        this.defaultRoutingKeyQueue = routingKeyName;
     }
 
     @Override
     public void publish(EventDto event) {
-        rabbitTemplate.convertAndSend(DEFAULT_EXCHANGE_NAME,DEFAULT_ROUTINGKEY_QUEUE, event,message -> {
+        rabbitTemplate.convertAndSend(defaultExchangeName,defaultRoutingKeyQueue, event,message -> {
             message.getMessageProperties().setCorrelationId(LocalDateTime.now().toString());
             return message;
         });
