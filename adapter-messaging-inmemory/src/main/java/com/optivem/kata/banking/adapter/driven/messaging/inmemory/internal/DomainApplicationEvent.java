9c2bdd1b7c040e87dddb0b404212a28ed8e3eb4c
Kalin Ivanov <kalin2578@gmail.com>
2023-10-11 15:01:33 +0300

    mark data as transient

--- adapter-messaging-inmemory/src/main/java/com/optivem/kata/banking/adapter/driven/messaging/inmemory/internal/DomainApplicationEvent.java
+++ adapter-messaging-inmemory/src/main/java/com/optivem/kata/banking/adapter/driven/messaging/inmemory/internal/DomainApplicationEvent.java
@@ -4,13 +4,14 @@ import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 
 /**
- * This class serves as the Event Object that to be puslished by ApplicationEventPublisher
+ * This class serves as the Event Object that to be published by ApplicationEventPublisher
  */
 @Getter
 public class DomainApplicationEvent<T> extends ApplicationEvent {
-    private T data;
+    private final transient T data;
 
-    public DomainApplicationEvent(T data) {
+    public DomainApplicationEvent(T data, T data1) {
         super(data);
+        this.data = data1;
     }
 }
