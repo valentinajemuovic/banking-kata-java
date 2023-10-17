78d92baadd7fb56b51105d096e3da5452134c745
Kalin Ivanov <kalin2578@gmail.com>
2023-10-17 17:21:06 +0300

    minor update

--- adapter-messaging-inmemory/src/main/java/com/optivem/kata/banking/adapter/driven/messaging/inmemory/ApplicationEventBus.java
+++ adapter-messaging-inmemory/src/main/java/com/optivem/kata/banking/adapter/driven/messaging/inmemory/ApplicationEventBus.java
@@ -1,16 +1,14 @@
 package com.optivem.kata.banking.adapter.driven.messaging.inmemory;
 
-
 import com.optivem.kata.banking.core.ports.driven.EventBus;
 import com.optivem.kata.banking.core.ports.driven.events.EventDto;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class ApplicationEventBus implements EventBus {
 
-    private ApplicationEventPublisher applicationEventPublisher;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     public ApplicationEventBus(ApplicationEventPublisher applicationEventPublisher){
         this.applicationEventPublisher = applicationEventPublisher;
