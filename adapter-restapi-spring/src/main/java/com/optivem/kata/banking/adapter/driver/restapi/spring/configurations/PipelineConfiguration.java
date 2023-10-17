c9509e56b4f5e472d85a0f036d188b822705c26c
Kalin Ivanov <kalin2578@gmail.com>
2023-10-17 15:09:56 +0300

    rename class

--- adapter-restapi-spring/src/main/java/com/optivem/kata/banking/adapter/driver/restapi/spring/configurations/PipelineConfiguration.java
+++ adapter-restapi-spring/src/main/java/com/optivem/kata/banking/adapter/driver/restapi/spring/configurations/PipelineConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class PipelinrConfiguration {
+public class PipelineConfiguration {
     @Bean
     public Pipeline pipeline(ObjectProvider<Command.Handler> commandHandlers,
                              ObjectProvider<Notification.Handler> notificationHandlers,
