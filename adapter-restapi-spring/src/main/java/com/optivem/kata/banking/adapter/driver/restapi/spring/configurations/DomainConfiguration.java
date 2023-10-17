80b927e37c35e1a41fa9b8f298b77f008b476aab
Kalin Ivanov <kalin2578@gmail.com>
2023-10-17 14:31:19 +0300

    inject dateTimeService field into score calculator

--- adapter-restapi-spring/src/main/java/com/optivem/kata/banking/adapter/driver/restapi/spring/configurations/DomainConfiguration.java
+++ adapter-restapi-spring/src/main/java/com/optivem/kata/banking/adapter/driver/restapi/spring/configurations/DomainConfiguration.java
@@ -3,18 +3,13 @@ package com.optivem.kata.banking.adapter.driver.restapi.spring.configurations;
 import com.optivem.kata.banking.core.internal.cleanarch.domain.scoring.DefaultScoreCalculator;
 import com.optivem.kata.banking.core.internal.cleanarch.domain.scoring.ScoreCalculator;
 import com.optivem.kata.banking.core.ports.driven.DateTimeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DomainConfiguration {
-
-    @Autowired
-    private DateTimeService dateTimeService;
-
     @Bean
-    public ScoreCalculator scoreCalculator() {
+    public ScoreCalculator scoreCalculator(DateTimeService dateTimeService) {
         return DefaultScoreCalculator.create(dateTimeService);
     }
 }
