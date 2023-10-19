1c1c1e46fbde4922a80f6771761a99f232d416e9
Kalin Ivanov <kalin2578@gmail.com>
2023-10-19 14:38:35 +0300

    added private constructor

--- core/src/main/java/com/optivem/kata/banking/core/internal/cleanarch/acl/FundsDepositedConverter.java
+++ core/src/main/java/com/optivem/kata/banking/core/internal/cleanarch/acl/FundsDepositedConverter.java
@@ -3,7 +3,10 @@ package com.optivem.kata.banking.core.internal.cleanarch.acl;
 import com.optivem.kata.banking.core.internal.cleanarch.domain.accounts.FundsDeposited;
 import com.optivem.kata.banking.core.ports.driven.events.FundsDepositedDto;
 
-public class FundsDepositedConverter {
+public final class FundsDepositedConverter {
+    private FundsDepositedConverter() {
+    }
+
     public static FundsDepositedDto fromEvent(FundsDeposited event) {
         return FundsDepositedDto.builder()
                 .timestamp(event.getTimestamp())
