45966e397fa4c964df63888974be521a7e717fbb
Kalin Ivanov <kalin2578@gmail.com>
2023-10-19 15:37:22 +0300

    added logger instead system out

--- z-thirdparty-provider-contracts/src/main/java/com/optivem/kata/banking/Main.java
+++ z-thirdparty-provider-contracts/src/main/java/com/optivem/kata/banking/Main.java
@@ -1,7 +1,11 @@
 package com.optivem.kata.banking;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LOGGER.info("Hello world!");
     }
 }
