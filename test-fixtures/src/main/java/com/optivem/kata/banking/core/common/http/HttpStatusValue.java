3e78c0ca4a43c32df2aa79b3816fb9cca4922cdb
Kalin Ivanov <kalin2578@gmail.com>
2023-10-19 15:28:57 +0300

    added private constructor

--- test-fixtures/src/main/java/com/optivem/kata/banking/core/common/http/HttpStatusValue.java
+++ test-fixtures/src/main/java/com/optivem/kata/banking/core/common/http/HttpStatusValue.java
@@ -2,7 +2,10 @@ package com.optivem.kata.banking.core.common.http;
 
 import org.springframework.http.HttpStatus;
 
-public class HttpStatusValue {
-    public static int NOT_FOUND = HttpStatus.NOT_FOUND.value();
-    public static int OK = HttpStatus.OK.value();
+public final class HttpStatusValue {
+    private HttpStatusValue() {
+    }
+
+    public static final int NOT_FOUND = HttpStatus.NOT_FOUND.value();
+    public static final int OK = HttpStatus.OK.value();
 }
