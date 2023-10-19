ee316949b835067f0b9eacf5186f534a82e0c876
Kalin Ivanov <kalin2578@gmail.com>
2023-10-19 15:26:52 +0300

    added private constructor

--- test-fixtures/src/main/java/com/optivem/kata/banking/core/common/http/HttpHost.java
+++ test-fixtures/src/main/java/com/optivem/kata/banking/core/common/http/HttpHost.java
@@ -1,5 +1,8 @@
 package com.optivem.kata.banking.core.common.http;
 
-public class HttpHost {
+public final class HttpHost {
+    private HttpHost() {
+    }
+
     public static final String LOCALHOST = "localhost";
 }
