d1123a813e8763d8d6074018936be9ee196149bf
Kalin Ivanov <kalin2578@gmail.com>
2023-10-19 14:39:54 +0300

    remove unused import in EventPublisher

--- core/src/main/java/com/optivem/kata/banking/core/internal/cleanarch/domain/common/events/EventPublisher.java
+++ core/src/main/java/com/optivem/kata/banking/core/internal/cleanarch/domain/common/events/EventPublisher.java
@@ -1,7 +1,5 @@
 package com.optivem.kata.banking.core.internal.cleanarch.domain.common.events;
 
-import com.optivem.kata.banking.core.internal.cleanarch.domain.common.events.DomainEvent;
-
 public interface EventPublisher {
     void publishEvent(DomainEvent event);
 }
