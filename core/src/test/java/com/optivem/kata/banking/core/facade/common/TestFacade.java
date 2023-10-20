ec906759b20fb264d2b2f6da533604793cde5176
Kalin Ivanov <kalin2578@gmail.com>
2023-10-20 11:36:05 +0300

    covert to a record TestFacade class

--- core/src/test/java/com/optivem/kata/banking/core/facade/common/TestFacade.java
+++ core/src/test/java/com/optivem/kata/banking/core/facade/common/TestFacade.java
@@ -2,17 +2,8 @@ package com.optivem.kata.banking.core.facade.common;
 
 import com.optivem.kata.banking.core.Facade;
 import com.optivem.kata.banking.core.common.builders.requests.OpenAccountRequestBuilder;
-import com.optivem.kata.banking.core.ports.driven.*;
 
-import java.time.LocalDateTime;
-
-public class TestFacade {
-
-    private final Facade facade;
-
-    public TestFacade(Facade facade) {
-        this.facade = facade;
-    }
+public record TestFacade(Facade facade) {
 
     public String alreadyHasBankAccount(int balance) {
         var openAccountRequest = OpenAccountRequestBuilder.openAccountRequest()
@@ -21,10 +12,5 @@ public class TestFacade {
 
         var openAccountResponse = facade.execute(openAccountRequest);
         return openAccountResponse.getAccountNumber();
-    }
-
-    // TODO: VC: This is temporary, re-design to remove this
-    public Facade getFacade() {
-        return facade;
     }
 }
