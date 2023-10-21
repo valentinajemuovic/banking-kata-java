ec758b4684e1820b4012e81def1f32ac0791d76e
Kalin Ivanov <kalin2578@gmail.com>
2023-10-21 21:03:36 +0300

    added simple tests

--- adapter-thirdparty-fake/src/test/java/com/optivem/kata/banking/adapter/driven/thirdparty/fake/FakeNationalIdentityGatewayTest.java
+++ adapter-thirdparty-fake/src/test/java/com/optivem/kata/banking/adapter/driven/thirdparty/fake/FakeNationalIdentityGatewayTest.java
@@ -2,6 +2,10 @@ package com.optivem.kata.banking.adapter.driven.thirdparty.fake;
 
 import com.optivem.kata.banking.adapter.driven.base.NationalIdentityGatewayTest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class FakeNationalIdentityGatewayTest extends NationalIdentityGatewayTest<FakeNationalIdentityGateway> {
 
@@ -13,5 +17,21 @@ public class FakeNationalIdentityGatewayTest extends NationalIdentityGatewayTest
     @Override
     protected void setupExistentUser(String nationalIdentityNumber) {
         provider.setupExistent(nationalIdentityNumber);
+    }
+
+    @Test
+    void shouldReturnTrueWithExistentUser() {
+        var existentNationalIdentityNumber = "12345";
+        setupExistentUser(existentNationalIdentityNumber);
+
+        var exists = provider.exists(existentNationalIdentityNumber);
+        assertTrue(exists);
+    }
+
+    @Test
+    void shouldReturnFalseWithNonExistentUser() {
+        var nonExistentNationalIdentityNumber = "67890";
+        var exists = provider.exists(nonExistentNationalIdentityNumber);
+        assertFalse(exists);
     }
 }
