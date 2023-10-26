2f94348c6ad29b4c6759168b3efb5f4022d898c3
Kalin Ivanov <kalin2578@gmail.com>
2023-10-26 16:29:29 +0300

    minor update

--- adapter-persistence-mongo/src/main/java/com/optivem/kata/banking/adapter/driven/persistence/mongo/internal/MongoTemplateCustomDataAccessor.java
+++ adapter-persistence-mongo/src/main/java/com/optivem/kata/banking/adapter/driven/persistence/mongo/internal/MongoTemplateCustomDataAccessor.java
@@ -3,22 +3,20 @@ package com.optivem.kata.banking.adapter.driven.persistence.mongo.internal;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoDatabase;
+import lombok.Getter;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MongoTemplateCustomDataAccessor {
     private final MongoClient mongoClient;
+    @Getter
     private final MongoTemplate mongoTemplate;
 
     public MongoTemplateCustomDataAccessor() {
         mongoClient = MongoClients.create(System.getenv("MONGODB_URI"));
         MongoDatabase database = mongoClient.getDatabase(System.getenv("MONGODB_DATABASE_NAME"));
         mongoTemplate = new MongoTemplate(mongoClient, database.getName());
-    }
-
-    public MongoTemplate getMongoTemplate() {
-        return mongoTemplate;
     }
 
     public void close() {
