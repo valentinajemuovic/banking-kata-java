b683e8fd745066c6c0247522a7da8f09c4c57044
Kalin Ivanov <kalin2578@gmail.com>
2023-10-26 15:56:33 +0300

    added getter annotation

--- adapter-messaging-rabbitmq/src/main/java/com/optivem/kata/banking/adminrabbitmq/AdminAMQP.java
+++ adapter-messaging-rabbitmq/src/main/java/com/optivem/kata/banking/adminrabbitmq/AdminAMQP.java
@@ -1,19 +1,17 @@
 package com.optivem.kata.banking.adminrabbitmq;
 
+import lombok.Getter;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.stereotype.Component;
 
+@Getter
 @Component
 public class AdminAMQP {
     private final RabbitAdmin rabbitAdmin;
 
     public AdminAMQP(RabbitAdmin rabbitAdmin){
         this.rabbitAdmin = rabbitAdmin;
-    }
-
-    public RabbitAdmin getRabbitAdmin(){
-        return this.rabbitAdmin;
     }
 
     public void createExchange(String exchangeName){
