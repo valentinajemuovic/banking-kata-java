8f2cf7fdf44892298e61338c317d343908699d44
Kalin Ivanov <kalin2578@gmail.com>
2023-10-28 11:16:08 +0300

    add tests

--- adapter-generation-fake/src/test/java/com/optivem/kata/banking/adapter/driven/generation/fake/FakeAccountNumberGeneratorTest.java
+++ adapter-generation-fake/src/test/java/com/optivem/kata/banking/adapter/driven/generation/fake/FakeAccountNumberGeneratorTest.java
@@ -1,6 +1,11 @@
 package com.optivem.kata.banking.adapter.driven.generation.fake;
 
 import com.optivem.kata.banking.adapter.driven.base.GeneratorTest;
+import com.optivem.kata.banking.adapter.driven.generation.fake.internal.NextElementIsNotSetupException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FakeAccountNumberGeneratorTest extends GeneratorTest<FakeAccountNumberGenerator> {
 
@@ -18,5 +23,33 @@ public class FakeAccountNumberGeneratorTest extends GeneratorTest<FakeAccountNum
         generator.setupNext(expectedValue3);
 
         return generator;
+    }
+
+    @Test
+    void shouldReturnsSetupValuesInOrder() {
+        var generator = create();
+
+        assertEquals("GB54BARC20032611545669", generator.next());
+        assertEquals("GB36BARC20038032622823", generator.next());
+        assertEquals("GB10BARC20040184197751", generator.next());
+    }
+
+    @Test
+    void shouldThrowsExceptionWhenNoMoreValues() {
+        var generator = create();
+        generator.next();
+        generator.next();
+        generator.next();
+
+        assertThrows(NextElementIsNotSetupException.class, generator::next);
+    }
+
+    @Test
+    void shouldAddsValuesToQueue() {
+        var generator = new FakeAccountNumberGenerator();
+        var newValue = "GB72BARC20041512345678";
+
+        generator.setupNext(newValue);
+        assertEquals(newValue, generator.next());
     }
 }
