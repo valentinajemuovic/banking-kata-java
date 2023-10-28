bb4a766802a20589c1ebd173d4a4005658a24a38
Kalin Ivanov <kalin2578@gmail.com>
2023-10-28 11:09:42 +0300

    add tests

--- adapter-persistence-jdbc/src/test/java/com/optivem/kata/banking/adapter/driven/persistence/jdbc/JdbcBankAccountStorageTest.java
+++ adapter-persistence-jdbc/src/test/java/com/optivem/kata/banking/adapter/driven/persistence/jdbc/JdbcBankAccountStorageTest.java
@@ -5,11 +5,21 @@ import com.optivem.kata.banking.adapter.driven.base.BankAccountStorageTest;
 import com.optivem.kata.banking.adapter.driven.base.ProfileNames;
 import com.optivem.kata.banking.core.ports.driven.AccountIdGenerator;
 import com.optivem.kata.banking.core.ports.driven.AccountNumberGenerator;
+import com.optivem.kata.banking.core.ports.driven.BankAccountDto;
 import com.optivem.kata.banking.core.ports.driven.BankAccountStorage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(classes = BankingApplication.class)
 @ActiveProfiles({ProfileNames.ADAPTER_PERSISTENCE_JDBC, ProfileNames.ADAPTER_GENERATION_RANDOM, ProfileNames.ADAPTER_TIME_SYSTEM, ProfileNames.ADAPTER_MICROSERVICE_SIM, ProfileNames.ADAPTER_THIRDPARTY_SIM})
@@ -18,5 +28,28 @@ public class JdbcBankAccountStorageTest extends BankAccountStorageTest {
     @Autowired
     public JdbcBankAccountStorageTest(BankAccountStorage storage, AccountIdGenerator accountIdGenerator, AccountNumberGenerator accountNumberGenerator) {
         super(storage, accountIdGenerator, accountNumberGenerator);
+    }
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private JdbcBankAccountStorage jdbcBankAccountStorage;
+
+    @BeforeEach
+    void init() {
+        jdbcBankAccountStorage = new JdbcBankAccountStorage(jdbcTemplate);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"12345", "67890"})
+    void shouldAddAndFind(String accountNumber) {
+        var bankAccountDto = new BankAccountDto();
+        bankAccountDto.setAccountNumber(accountNumber);
+
+        jdbcBankAccountStorage.add(bankAccountDto);
+
+        Optional<BankAccountDto> foundAccountDto = jdbcBankAccountStorage.find(accountNumber);
+        assertTrue(foundAccountDto.isPresent());
+        assertEquals(accountNumber, foundAccountDto.get().getAccountNumber());
     }
 }
