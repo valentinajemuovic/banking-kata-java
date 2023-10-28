445093aa68d4a68aeceb3fdf3ed61426ab299505
Kalin Ivanov <kalin2578@gmail.com>
2023-10-28 11:07:47 +0300

    minor update

--- adapter-persistence-redis/src/test/java/com/optivem/kata/banking/adapter/driven/persistence/redis/RedisBankAccountStorageTest.java
+++ adapter-persistence-redis/src/test/java/com/optivem/kata/banking/adapter/driven/persistence/redis/RedisBankAccountStorageTest.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest(classes = BankingApplication.class)
 // @ContextConfiguration // TODO: VC: Probably ring this back and delete ActiveProfiles; ActiveProfiles was temporarily added
